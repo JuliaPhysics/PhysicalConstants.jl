3bb34fd66ad9a207e2a31a3d3b2b5a8ae25a577a
Mosè Giordano <mose@gnu.org>
2018-09-08 21:19:55 +0200

    Add more constants

--- src/Constants.jl
+++ src/Constants.jl
@@ -2,8 +2,6 @@ module Constants
 
 using Measurements, Unitful
 
-export @constant
-
 struct Constant{sym} <: Number end
 
 function name end
@@ -62,35 +60,68 @@ macro constant(sym, name, val, def, unit, unc, bigunc, reference)
         Unitful.unit(::Constant{$qsym})      = $unit
         Unitful.dimension(::Constant{$qsym}) = Unitful.dimension($unit)
 
-        @assert isa(float($esym).val, Float64)
-        @assert isa(big($esym).val, BigFloat)
-        @assert isa(measurement($esym).val, Measurement{Float64})
-        @assert float(Float64, $esym).val == Float64(big($esym).val)
-        @assert float(Float32, $esym).val == Float32(big($esym).val)
+        @assert isa(ustrip(float($esym)), Float64)
+        @assert isa(ustrip(big($esym)), BigFloat)
+        @assert isa(ustrip(measurement($esym)), Measurement{Float64})
+        @assert ustrip(float(Float64, $esym)) == Float64(ustrip(big($esym)))
+        @assert ustrip(float(Float32, $esym)) == Float32(ustrip(big($esym)))
+        @assert Float64(ustrip(measurement(BigFloat, $esym)).err) ==
+            ustrip(measurement($esym)).err
     end
 end
 
-function Base.show(io::IO, x::Constant)
-    println(io, "Name        = ", name(x))
-    println(io, "Value       = ", float(x).val)
-    println(io, "Uncertainty = ", measurement(x).val.err)
-    println(io, "Unit        = ", unit(x))
-    print(io,   "Reference   = ", ref(x))
+macro derived_constant(sym, name, val, def, unit, measure64, measurebig, reference)
+    esym = esc(sym)
+    qsym = esc(Expr(:quote, sym))
+    eunit = esc(unit)
+    tag = Measurements.tag_counters[Base.Threads.threadid()] += 1
+    _bigconvert = isa(def,Symbol) ? quote
+        function _big(::Constant{$qsym})
+            c = BigFloat()
+            ccall(($(string("mpfr_const_", def)), :libmpfr),
+                  Cint, (Ref{BigFloat}, Int32), c, MPFR.ROUNDING_MODE[])
+            return c
+        end
+    end : quote
+        _big(::Constant{$qsym}) = $(esc(def))
+    end
+    quote
+        const $esym = Constant{$qsym}()
+        Base.float(::Constant{$qsym}) = $val * $unit
+        Base.float(FT::DataType, ::Constant{$qsym}) = FT($val) * $eunit
+        $_bigconvert
+        Base.big(x::Constant{$qsym}) = _big(x) * $eunit
+        Base.float(::Type{BigFloat}, x::Constant{$qsym}) = big(x)
+
+        Measurements.measurement(::Type{Float64}, ::Constant{$qsym}) = $(esc(measure64))
+        Measurements.measurement(::Type{BigFloat}, ::Constant{$qsym}) = $(esc(measurebig))
+        Measurements.measurement(::Constant{$qsym}) = measurement(Float64, $esym)
+
+        Constants.name(::Constant{$qsym})    = $name
+        Constants.ref(::Constant{$qsym})     = $reference
+        Unitful.unit(::Constant{$qsym})      = $unit
+        Unitful.dimension(::Constant{$qsym}) = Unitful.dimension($unit)
+
+        @assert isa(ustrip(float($esym)), Float64)
+        @assert isa(ustrip(big($esym)), BigFloat)
+        @assert isa(ustrip(measurement($esym)), Measurement{Float64})
+        @assert ustrip(float(Float64, $esym)) == Float64(ustrip(big($esym)))
+        @assert ustrip(float(Float32, $esym)) == Float32(ustrip(big($esym)))
+        @assert Float64(ustrip(measurement(BigFloat, $esym)).err) ==
+            ustrip(measurement($esym)).err
+    end
 end
 
-module CODATA2014
-
-using Constants, Unitful
-
-export c, h
-
-@constant(c, "Speed of light in vacuum", 299_792_458.0, BigFloat(299_792_458.0), u"m/s",
-          0.0, BigFloat(0), "CODATA 2014")
-@constant(h, "Planck constant", 6.626_070_040e-34,
-          6_626_070_040/10000000000000000000000000000000000000000000,
-          u"J*s", 8.1e-42, 81/10000000000000000000000000000000000000000000,
-          "CODATA 2014")
-
+function Base.show(io::IO, x::Constant{sym}) where sym
+    println(io, "$(name(x)) ($sym)")
+    println(io, "Value                         = ", float(x))
+    println(io, "Standard uncertainty          = ", iszero(ustrip(measurement(x)).err) ? "(exact)" :
+            ustrip(measurement(x)).err * unit(x))
+    println(io, "Relative standard uncertainty = ", iszero(ustrip(measurement(x)).err) ? "(exact)" :
+            round(ustrip(measurement(x)).err/ustrip(measurement(x)).val, sigdigits=2))
+    print(io,   "Reference                     = ", ref(x))
 end
+
+include("codata2014.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
-using Constants
+using Constants, Measurements, Unitful
 using Test
 
 using Constants.CODATA2014
 
 @testset begin
-    @test big(h).val == big"6.626070040e-34"
-    @test setprecision(BigFloat, 768) do; precision(big(c).val) end == 768
+    @test ustrip(big(h)) == big"6.626070040e-34"
+    @test setprecision(BigFloat, 768) do; precision(ustrip(big(c))) end == 768
     @test measurement(h) === measurement(h)
-    @test iszero(measurement(h) - measurement(h))
-    @test isone(measurement(BigFloat, c) / measurement(BigFloat, c))
+    @test iszero(measurement(α) - measurement(α))
+    @test isone(measurement(BigFloat, atm) / measurement(BigFloat, atm))
+    @test iszero(measurement(BigFloat, ħ) - (measurement(BigFloat, h) / 2big(pi)))
+    @test isone(measurement(BigFloat, ħ) / (measurement(BigFloat, h) / 2big(pi)))
 end
