34d61ec5b67c8060121bcea2717a9d0902f9eb9f
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-03-27 22:58:44 +0000

    Test show methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,17 +11,6 @@ using Test
     @test isone(measurement(BigFloat, ħ) / (measurement(BigFloat, h) / 2big(pi)))
 end
 
-@testset "Maths" begin
-    @test α ≈ @inferred(e^2/(4 * pi * ε_0 * ħ * c))
-    @test @inferred(α + 2) ≈ 2 + float(α)
-    @test @inferred(5 + α) ≈ float(α) + 5
-    @test @inferred(α + 2.718) ≈ 2.718 + float(α)
-    @test @inferred(-3.14 + α) ≈ float(α) - 3.14
-    @test ε_0 ≈ @inferred(1 / (μ_0 * c ^ 2))
-    @test @inferred(big(0) + α) == big(α)
-    @test α * 1.0 == float(α)
-end
-
 @testset "Promotion" begin
     x = @inferred(inv(μ_0 * c ^ 2))
     T = promote_type(typeof(ε_0), typeof(x))
@@ -32,4 +21,36 @@ end
     @test convert(T, ε_0) === (ε_0 / unit(ε_0)) * u
     @test convert(Float32, α) === float(Float32, α)
     @test uconvert(u"cm/s", c) === uconvert(u"cm/s", float(c))
+end
+
+@testset "Maths" begin
+    @test α ≈ @inferred(e^2/(4 * pi * ε_0 * ħ * c))
+    @test @inferred(α + 2) ≈ 2 + float(α)
+    @test @inferred(5 + α) ≈ float(α) + 5
+    @test @inferred(α + 2.718) ≈ 2.718 + float(α)
+    @test @inferred(-3.14 + α) ≈ float(α) - 3.14
+    @test ε_0 ≈ @inferred(1 / (μ_0 * c ^ 2))
+    @test @inferred(big(0) + α) == big(α)
+    @test @inferred(α * 1.0) == float(α)
+end
+
+@testset "Show" begin
+    @test repr(c) ==
+        "Speed of light in vacuum (c)
+Value                         = 2.99792458e8 m s^-1
+Standard uncertainty          = (exact)
+Relative standard uncertainty = (exact)
+Reference                     = CODATA 2014"
+    @test repr(α) ==
+        "Fine-structure constant (α)
+Value                         = 0.0072973525664
+Standard uncertainty          = 1.7e-12
+Relative standard uncertainty = 2.3e-10
+Reference                     = CODATA 2014"
+    @test repr(e) ==
+        "Elementary charge (e)
+Value                         = 1.6021766208e-19 C
+Standard uncertainty          = 9.8e-28 C
+Relative standard uncertainty = 6.1e-9
+Reference                     = CODATA 2014"
 end
